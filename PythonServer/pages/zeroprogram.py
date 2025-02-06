82b3b3589c7ab023341625476b0459bbcfa247a5
ShawnNeff <shawnneff73@gmail.com>
2025-02-05 22:36:27 -0800

    Update zeroprogram.py

--- PythonServer/pages/zeroprogram.py
+++ PythonServer/pages/zeroprogram.py
@@ -5,17 +5,9 @@ import openpyxl as op
 import io
 import xlsxwriter
 
-
 st.set_page_config(layout="wide")
 
-# st.sidebar.image('logo.png', width=260)
-
-# st.sidebar.page_link("main.py", label="Home")
-# st.sidebar.page_link("Pages/inboundreports.py", label="Inventory Reports")
-# st.sidebar.page_link("Pages/nobinzerolist.py", label="NOBIN / ZERO Lists")
-# st.sidebar.page_link("Pages/3dayold.py", label="3 Day old ASN's")
-# st.sidebar.page_link("Pages/zeroprogram.py", label="Zero Program")
-# st.sidebar.page_link("Pages/itemclasssize.py", label="Calculate Item Class Size")
+descision = st.radio("Information you want to see:", ["Down to Zero", "Every Transaction"], index=None)
 
 onhand = st.text_input("Enter stock on hand: ")
 
@@ -65,9 +57,10 @@ if item is not None and onhand != "":
             if type not in returns:
                 quantity = int(file.loc[i, 'Quantity']) - quantity
         
-        if quantity == 0:
+        if descision == "Down to Zero" and quantity == 0:
             i += 1
             file.loc[i, 'OnHand'] = quantity
+            
             while l > i:
                 file = file.drop(l)
                 l -= 1
