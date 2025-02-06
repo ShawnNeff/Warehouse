d2a6ad8eef36745db1a6015eaaa9adc979a4f90b
ShawnNeff <shawnneff73@gmail.com>
2025-02-05 22:48:20 -0800

    Update main.py

--- PythonServer/main.py
+++ PythonServer/main.py
@@ -6,10 +6,3 @@ import io
 import xlsxwriter
 
 st.set_page_config(layout="wide")
-
-st.sidebar.page_link("main.py", label="Home")
-st.sidebar.page_link("Pages/inboundreports.py", label="Inventory Reports")
-st.sidebar.page_link("Pages/nobinzerolist.py", label="NOBIN / ZERO Lists")
-st.sidebar.page_link("Pages/3dayold.py", label="3 Day old ASN's")
-st.sidebar.page_link("Pages/zeroprogram.py", label="Zero Program")
-st.sidebar.page_link("Pages/itemclasssize.py", label="Calculate Item Class Size")
