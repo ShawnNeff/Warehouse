5d8ca7efaef7e48affe1f5a73922d31ae18b57ca
ShawnNeff <shawnneff73@gmail.com>
2025-02-06 19:09:30 -0800

    Update main.py

--- PythonServer/main.py
+++ PythonServer/main.py
@@ -4,6 +4,9 @@ import pandas as pd
 import openpyxl as op
 import io
 import xlsxwriter
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 st.set_page_config(layout="wide")
 st.title("Coming Soon")
