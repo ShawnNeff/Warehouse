7a20baae65925ec67efb8ec0c779034af3600e7a
ShawnNeff <shawnneff73@gmail.com>
2025-02-26 23:15:27 -0800

    Update 6_Bin_Change_Log.py

--- PythonServer/pages/6_Bin_Change_Log.py
+++ PythonServer/pages/6_Bin_Change_Log.py
@@ -7,15 +7,15 @@ import xlsxwriter
 
 st.set_page_config(layout="wide")
 st.title("Bin Change Log Report")
-st.sidebar.success("Reliable Parts")
-with st.sidebar:
-    st.page_link('./main.py', label="Home")
-    st.page_link('./pages/2_Inventory_History.py', label="Inventory History")
-    st.page_link('./pages/3_Inventory_Reports.py', label="Inbound Reports")
-    st.page_link('./pages/4_Nobin_Zero_Lists.py', label="Nobin / Zero Reports")
-    st.page_link('./pages/5_Three_Day_Old_ASN_Reports.py', label="ASN 3 Day Old Report")
-    st.page_link('./pages/6_Bin_Change_Log.py', label="Item Bin Size Change")
-    st.page_link('./pages/7_Calculate_Item_Class_Size.py', label="Calculate Item Class Size")
+# st.sidebar.success("Reliable Parts")
+# with st.sidebar:
+#     st.page_link('./main.py', label="Home")
+#     st.page_link('./pages/2_Inventory_History.py', label="Inventory History")
+#     st.page_link('./pages/3_Inventory_Reports.py', label="Inbound Reports")
+#     st.page_link('./pages/4_Nobin_Zero_Lists.py', label="Nobin / Zero Reports")
+#     st.page_link('./pages/5_Three_Day_Old_ASN_Reports.py', label="ASN 3 Day Old Report")
+#     st.page_link('./pages/6_Bin_Change_Log.py', label="Item Bin Size Change")
+#     st.page_link('./pages/7_Calculate_Item_Class_Size.py', label="Calculate Item Class Size")
 
 file1 = st.file_uploader("**Inventory File** - Upload inventory excel file in xlsx format.",type="xlsx")
 # file2 = st.file_uploader("**Bin Change Log** - Upload bin change log excel file in xlsx format.",type="xlsx")
