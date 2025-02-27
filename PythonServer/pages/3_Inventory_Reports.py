80158421b4ad182a8e6a1e32fb857f75baa0abec
ShawnNeff <shawnneff73@gmail.com>
2025-02-26 23:10:03 -0800

    Update 3_Inventory_Reports.py

--- PythonServer/pages/3_Inventory_Reports.py
+++ PythonServer/pages/3_Inventory_Reports.py
@@ -16,7 +16,16 @@ def fill_lists(i, imax, dfloc):
 
 st.set_page_config(layout="wide")
 st.title("Inventory Reports")
-
+st.sidebar.success("Reliable Parts")
+with st.sidebar:
+    st.page_link('./main.py', label="Home")
+    st.page_link('./pages/2_Inventory_History.py', label="Inventory History")
+    st.page_link('./pages/3_Inventory_Reports.py', label="Inbound Reports")
+    st.page_link('./pages/4_Nobin_Zero_Lists.py', label="Nobin / Zero Reports")
+    st.page_link('./pages/5_Three_Day_Old_ASN_Report.py', label="ASN 3 Day Old Report")
+    st.page_link('./pages/6_Bin_Change_Log.py', label="Item Bin Size Change")
+    st.page_link('./pages/7_Calculate_Item_Class_Size.py', label="Calculate Item Class Size")
+    
 file = st.file_uploader("**Inventory File** - Upload inventory excel file in xlsx format.",type="xlsx")
 file2 = st.file_uploader("**Multiple Bins** - Upload excel file with all multi-locations in xlsx format.", type="xlsx")
 
