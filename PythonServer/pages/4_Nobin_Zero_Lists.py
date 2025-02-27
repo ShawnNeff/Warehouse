90100a36d0b1d37f7c164bf13c14b262f79c7c1f
ShawnNeff <shawnneff73@gmail.com>
2025-02-26 23:15:00 -0800

    Update 4_Nobin_Zero_Lists.py

--- PythonServer/pages/4_Nobin_Zero_Lists.py
+++ PythonServer/pages/4_Nobin_Zero_Lists.py
@@ -18,15 +18,15 @@ def checknobin(file1, file2):
 
 st.set_page_config(layout="wide")
 st.title("NOBIN / ZERO Lists")
-st.sidebar.success("Reliable Parts")
-with st.sidebar:
-    st.page_link('./main.py', label="Home")
-    st.page_link('./pages/2_Inventory_History.py', label="Inventory History")
-    st.page_link('./pages/3_Inventory_Reports.py', label="Inbound Reports")
-    st.page_link('./pages/4_Nobin_Zero_Lists.py', label="Nobin / Zero Reports")
-    st.page_link('./pages/5_Three_Day_Old_ASN_Reports.py', label="ASN 3 Day Old Report")
-    st.page_link('./pages/6_Bin_Change_Log.py', label="Item Bin Size Change")
-    st.page_link('./pages/7_Calculate_Item_Class_Size.py', label="Calculate Item Class Size")
+# st.sidebar.success("Reliable Parts")
+# with st.sidebar:
+#     st.page_link('./main.py', label="Home")
+#     st.page_link('./pages/2_Inventory_History.py', label="Inventory History")
+#     st.page_link('./pages/3_Inventory_Reports.py', label="Inbound Reports")
+#     st.page_link('./pages/4_Nobin_Zero_Lists.py', label="Nobin / Zero Reports")
+#     st.page_link('./pages/5_Three_Day_Old_ASN_Reports.py', label="ASN 3 Day Old Report")
+#     st.page_link('./pages/6_Bin_Change_Log.py', label="Item Bin Size Change")
+#     st.page_link('./pages/7_Calculate_Item_Class_Size.py', label="Calculate Item Class Size")
 
 file = st.file_uploader("**Nobin Master** - Upload nobin master file.",type="xlsx")
 file2 = st.file_uploader("**Nobin List** - Upload nobin list file.", type="xlsx")
