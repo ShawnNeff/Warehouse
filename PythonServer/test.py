d69dbea4516830f5ba2901e60f42e4e331a92f0b
ShawnNeff <shawnneff73@gmail.com>
2026-06-01 10:23:23 -0700

    Delete PythonServer/test.py

--- PythonServer/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-import openpyxl as op
-import xlsxwriter
-
-file = pd.read_excel('Transactions.xls')
-
-file.dropna(subset=['Unnamed: 0'], inplace=True)
-file.to_excel('result.xlsx', index=False)
