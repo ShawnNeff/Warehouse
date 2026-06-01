7b13731176c544fa84665788cbe6a8a5050fdf92
ShawnNeff <shawnneff73@gmail.com>
2026-06-01 10:39:09 -0700

    Update inventoryhistory.py

--- PythonServer/views/inventoryhistory.py
+++ PythonServer/views/inventoryhistory.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import os
 import pandas as pd
-import openpyxl as op
+#import openpyxl as op
 import io
-import xlsxwriter
+#import xlsxwriter
     
 descision = st.radio("Information you want to see:", ["Down to Zero", "Every Transaction"], index=None)
 
