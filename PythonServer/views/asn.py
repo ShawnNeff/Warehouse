a1ac8fa89024b6a519717ec9b070696257682215
ShawnNeff <shawnneff73@gmail.com>
2026-06-01 12:44:12 -0700

    Update asn.py

--- PythonServer/views/asn.py
+++ PythonServer/views/asn.py
@@ -9,8 +9,9 @@ import xlsxwriter
 def clean_data(file):
     temp = file.drop(file.columns[[0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25]], axis=1, inplace=True)
     #temp.columns = ['ASN', 'CONSOLIDATED']
-    file = temp.dropna()
-    return file
+    #file = temp.dropna()
+    #return file
+    return temp
 
 # Function - returns set of all ASN's in the file
 def get_asn(file):
