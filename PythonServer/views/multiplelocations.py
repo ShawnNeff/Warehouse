c2e1daea109b7a16d79991348e5c1a783c84e703
ShawnNeff <shawnneff73@gmail.com>
2026-06-02 09:34:08 -0700

    Update multiplelocations.py

--- PythonServer/views/multiplelocations.py
+++ PythonServer/views/multiplelocations.py
@@ -1,1 +1,17 @@
 import streamlit as st
+import pandas as pd
+
+item = st.file_uploader("Upload Inventory file.",type="xlsx")
+
+l = ['L PALLET', 'XL PALLET', 'S PALLET', 'L BIN 6', 'M SHELF', 'S SHELF', 'L SHELF', 'LONG']
+
+if item is not None:
+        
+    # open inventory file
+    file = pd.read_excel(item)
+
+    file[file.duplicated(subset=['column_name'], keep=False)]
+
+    for index, row in file.iterrows():
+        if row['BinSizeClassID'] not in l:
+            st.write(row)
