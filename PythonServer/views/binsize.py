3e0e82103a07288fb6c99bb744bad39d09caa545
ShawnNeff <shawnneff73@gmail.com>
2026-06-02 15:01:41 -0700

    Update binsize.py

--- PythonServer/views/binsize.py
+++ PythonServer/views/binsize.py
@@ -1,1 +1,15 @@
 import streamlit as st
+import pandas as pd
+
+item = st.file_uploader("Upload inventory file.",type="xlsx")
+
+if item is not None:
+    file = pd.read_excel(item)
+    item_list = [["", ""]]
+
+
+    for index, row in file.iterrows():
+        if row['ItemSizeClassID'] != row['BinSizeClassID']:
+            item_list.append([row['ItemID'], row['BinSizeClassID']])
+    
+    st.write(item_list)
