8f8fefda2f5141a96d1f7ff706a51ecee5124520
ShawnNeff <shawnneff73@gmail.com>
2026-06-02 10:52:06 -0700

    Update asn.py

--- PythonServer/views/asn.py
+++ PythonServer/views/asn.py
@@ -32,7 +32,7 @@ def get_unique_asn(file, file2):
 def sort_set(file):
     return sorted(file)
 
-st.header("--- ASN Research - 3 Day Folder Tool ---")
+st.header("--- ASN Research - ASN's Removed from Yesterday's File ---")
 st.write("")
 st.write("")
 
