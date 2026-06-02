f4c0c830b26ac7592713e7e170487cc7c797cdb2
ShawnNeff <shawnneff73@gmail.com>
2026-06-02 09:36:38 -0700

    Update zero.py

--- PythonServer/views/zero.py
+++ PythonServer/views/zero.py
@@ -1,1 +1,24 @@
 import streamlit as st
+import pandas as pd
+
+zeromaster = pd.read_excel('zeromaster.xlsx')
+
+item = st.file_uploader("Upload inventory file.",type="xlsx")
+
+if item is not None:
+
+    file = pd.read_excel(item)
+
+    file2 = file[file['Quantity'] == 0]
+
+    file2.to_excel('zerolist.xlsx', index=False)
+
+    file = pd.read_excel('zerolist.xlsx')
+    file2 = pd.read_excel('zeromaster.xlsx')
+
+    file3 = file['ItemID'].isin(file2['ItemID'])
+    file.drop(file2[file3].index, inplace=True)
+
+    st.write(file)
+
+    #file.to_excel('zeromaster.xlsx', index=False)
