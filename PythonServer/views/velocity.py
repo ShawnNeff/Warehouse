f744cf08557d5b43047994ae7ca2f07a29003860
ShawnNeff <shawnneff73@gmail.com>
2026-06-02 15:21:21 -0700

    Update velocity.py

--- PythonServer/views/velocity.py
+++ PythonServer/views/velocity.py
@@ -1,1 +1,11 @@
 import streamlit as st
+import pandas as pd
+
+item = st.file_uploader("Upload inventory file.",type="xlsx")
+
+if item is not None:
+    file = pd.read_excel(item)
+
+    df = file[file['ItemVelocityClassID'] != file['BinVelocityClassID']]
+
+    st.write(df[['ItemID', 'BinVelocityClassID', 'ItemVelocityClassID']])
