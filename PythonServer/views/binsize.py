b3676f28ed0665ffc66e13decf8230b7ac4ba768
ShawnNeff <shawnneff73@gmail.com>
2026-06-02 15:12:17 -0700

    Update binsize.py

--- PythonServer/views/binsize.py
+++ PythonServer/views/binsize.py
@@ -5,11 +5,14 @@ item = st.file_uploader("Upload inventory file.",type="xlsx")
 
 if item is not None:
     file = pd.read_excel(item)
-    item_list = [["", ""]]
+    # item_list = [["", ""]]
 
-
-    for index, row in file.iterrows():
-        if row['ItemSizeClassID'] != row['BinSizeClassID']:
-            item_list.append([row['ItemID'], row['BinSizeClassID']])
+    # for index, row in file.iterrows():
+    #     if row['ItemSizeClassID'] != row['BinSizeClassID']:
+    #         item_list.append([row['ItemID'], row['BinSizeClassID']])
     
-    st.write(item_list)
+    # st.write(item_list)
+
+    df = file[file['ItemSizeClassID'] != file['BinSizeClassID']]
+
+    st.write(df)
