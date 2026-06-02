8b86d4c8ad28289c953b2e927ba8c898d0d79411
ShawnNeff <shawnneff73@gmail.com>
2026-06-02 09:34:36 -0700

    Update nobin.py

--- PythonServer/views/nobin.py
+++ PythonServer/views/nobin.py
@@ -1,1 +1,22 @@
 import streamlit as st
+import pandas as pd
+
+# list for all non locations
+
+nobinmaster = pd.read_excel('nobinmaster.xlsx')
+
+item = st.file_uploader("Upload unassigned bins file.",type="xlsx")
+
+if item is not None:
+
+    file = pd.read_excel(item)
+
+    # Combine both dataframes vertically
+    combined_file = pd.concat([file, nobinmaster])
+
+    # keep=False drops ALL occurrences of any rows that have matches
+    unique_combined_file = combined_file.drop_duplicates(keep=False)
+
+    st.write(unique_combined_file)
+
+    file.to_excel('nobinmaster.xlsx', index=False)
