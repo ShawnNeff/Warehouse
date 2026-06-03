3e1e1f19b51975c80fc9f20b1bc62054299a901a
ShawnNeff <shawnneff73@gmail.com>
2026-06-03 09:08:42 -0700

    Update binsize.py

--- PythonServer/views/binsize.py
+++ PythonServer/views/binsize.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+st.header("Item Bin Size Missmatch")
+st.write("")
+st.write("")
+
 item = st.file_uploader("Upload inventory file.",type="xlsx")
 
 if item is not None:
