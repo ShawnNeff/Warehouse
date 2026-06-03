8333ea08b1406f01beec6c163b81a09a93f63eba
ShawnNeff <shawnneff73@gmail.com>
2026-06-02 20:38:05 -0700

    Update multiplelocations.py

--- PythonServer/views/multiplelocations.py
+++ PythonServer/views/multiplelocations.py
@@ -14,16 +14,6 @@ if item is not None:
     # open inventory file
     file = pd.read_excel(item)
 
-    file = file[file.duplicated(subset=['PrimaryBin'], keep=False)]
+    file = file[file.duplicated(subset=['ItemID'], keep=False)]
 
-    for index, row in file.iterrows():
-        if row['BinSizeClassID'] in l:
-            file = file.drop(index)
-
-    # for index, row in file.iterrows():
-    #     index2 = index + 1
-    #     if file.loc[index2, 'ItemID'] in file.loc[index, 'ItemID']
-    #         file = file.drop(index2)
-    #         file = file.drop(index)
-        
-    st.write(file)
+    st.write(file[['ItemID', 'PrimaryBin', 'Quantity']])
