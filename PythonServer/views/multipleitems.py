482218c0649f0db018c81ab509a1a0d23c63f08a
ShawnNeff <shawnneff73@gmail.com>
2026-06-03 09:37:43 -0700

    Update multipleitems.py

--- PythonServer/views/multipleitems.py
+++ PythonServer/views/multipleitems.py
@@ -57,11 +57,11 @@ if item is not None:
             "RCVPLT51", "RCVPLT52", "RCVPLT53", "RCVPLT54", "RCVPLT55", "RCVPLT56", "RCVPLT57", 
             "RCVPLT58", "RCVPLT59", "RCVSMALL1", "RCVSMALL2", "RCVSMALL3", "RCVSMALL4")
 
-    # file.drop(file.columns[[
-    #     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23,
-    #     24, 25, 26, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
-    #     45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 62, 63, 64
-    # ]], axis=1, inplace=True)
+    file.drop(file.columns[[
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23,
+        24, 25, 26, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
+        45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 62, 63, 64
+    ]], axis=1, inplace=True)
 
     length = len(file) - 1
 
@@ -74,4 +74,4 @@ if item is not None:
 
     file = MultiItems(file)
 
-    st.write(file[['ItemID', 'Quanitty', 'PrimaryBin', 'BinID']])
+    st.write(file)
