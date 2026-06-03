7e482860198c2880ff0b98461cff64ac35abd11b
ShawnNeff <shawnneff73@gmail.com>
2026-06-03 15:19:04 -0700

    Update index.py

--- PythonServer/views/index.py
+++ PythonServer/views/index.py
@@ -1,1 +1,11 @@
 import streamlit as st
+
+st.header("Morning Checklist")
+st.write("")
+st.write("Make sure all carts return carts and P3 carts are put away.")
+st.write("P3RACK1A, P3RACK1B, P3RACK1C, P3RACK1D, P3RACK3, RTNCRT2, RTNCRT3, RTNPLT1, RTNFRONT")
+st.write("FCRTN - these are all customer discrepancies, just needs to be adjusted out of stock")
+st.write("Check ASN Folder - file anything outdated.")
+st.write("Check Oustanding Transfer Receipts - file anything over 10 days if not filed already.")
+st.write("")
+st.header("End of Night Checklist")
