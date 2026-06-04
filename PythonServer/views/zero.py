2e350a4506864dbd03f7af4c42e47afdd71a31d9
ShawnNeff <shawnneff73@gmail.com>
2026-06-04 09:44:48 -0700

    Update zero.py

--- PythonServer/views/zero.py
+++ PythonServer/views/zero.py
@@ -20,13 +20,13 @@ if item is not None:
 
     file2 = file[file['Quantity'] == 0]
 
-    file2.to_excel('zerolist.xlsx', index=False)
+    file2.to_excel('./PythonServer/files/zerolist.xlsx', index=False)
 
-    file = pd.read_excel('zerolist.xlsx')
-    file2 = pd.read_excel('zeromaster.xlsx')
+    file = pd.read_excel('./PythonServer/files/zerolist.xlsx')
+    #file2 = pd.read_excel('zeromaster.xlsx')
 
-    file3 = file['ItemID'].isin(file2['ItemID'])
-    file.drop(file2[file3].index, inplace=True)
+    file3 = file['ItemID'].isin(zeromaster['ItemID'])
+    file.drop(zeromaster[file3].index, inplace=True)
 
     st.write(file)
 
