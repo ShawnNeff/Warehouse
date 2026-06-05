0e16d8b6cb0f8d3e10efe7554c9b190e29e9a77e
ShawnNeff <shawnneff73@gmail.com>
2026-06-05 10:25:49 -0700

    Update transaction.py

--- PythonServer/views/transaction.py
+++ PythonServer/views/transaction.py
@@ -1,5 +1,5 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from datetime import datetime, date, timedelta
 
 # Function remove blank rows
@@ -333,124 +333,128 @@ def add_breaktwo(employee, master_user):
 
     return master_user
 
-# Varialbe - set of all users
-users = ("AJB002", "ALH002", "AXG004", "AXG006", "BBN001", "BJH001", "CXH003", "CXP001", "DLJ001", "DVA001", "DXN001", "DXK001", "EAY001", "JJS001", "JXA001", "KAN001", "LJM001", "MDL001", "NXP003", "PXL001", "SC303", "SC304", "SC306", "SPN001", "SXD001", "TXF001")
-# Variable - set for all return carts, p3 carts, and bagntag cart
-carts = ("#ACRT2A", "#ACRT2B", "#ACRT2C", "#ACRT2D", "#ACRT3A", "#ACRT3B", "#ACRT3C", "#ACRT3D", "RTNPLT1", "RTNFRONT", "P3RACK1A", "P3RACK1B", "P3RACK1C", "P3RACK1D", "#P3BINAA", "#P3BINAB", "#P3BINAC", "#P3BINBA", "#P3BINBB", "#P3BINBC", "#P3BINBD", "#P3BINCA", "#P3BINCB", "#P3BINCC", "#P3BINCD", "#P3BINDA", "#P3BINDB", "#P3BINDC", "#P3BINDD", "#P3BINDE", "#P3BINEA", "#P3BINEB", "#P3BINEC", "#P3BINED", "#P3BINEE", "#P3BINEF", "#P3BINFA", "#P3BINFB", "#P3BINFC", "#P3BINFD", "#P3BINFE", "#P3BINFF", "#P3BINFG", "#P3BINFH", "#P3BINGA", "#P3BINGB", "#P3BINGC", "#P3BINGD", "#P3BINGE", "#P3BINGF", "#P3BINGG", "#P3BINGH", "#P3BINHA", "#P3BINHB", "#P3BINHC", "#P3BINHD", "#P3BINHE", "#P3BINHG", "#P3BINHH", "#P3BINHI", "#P3BINHJ", "#P3BINHK", "#P3BINHL", "#P3BINHM", "#P3BINHN", "#P3BINHO", "#P3BINIA", "#P3BINIB", "#P3BINIC", "#P3BINID", "#P3BINIE", "#P3BINIF", "#P3BINIG", "#P3BINIH", "#P3BINII", "#P3BINIJ", "#P3BINIK", "#P3BINIL", "#P3BINIM", "#P3BININ", "#P3BINIO")
-# Variable - set for bagntag locations
-bagntag = ("#ACBAT", "#ACDMG", "#ACLABEL", "#ACOTHER")
+item = st.file_uploader("Upload transcations file to clean.",type="xlsx")
 
-# Variable - used to add row to user dataframe
-row_to_copy = []
-# Variable - list to hold user indexs
-user_rows_cleaned = []
-# Vailable - dictionary to store employee time
-employee_template = {
-    "clockin": "",
-    "lunchout": 0,
-    "lunchin": 0,
-    "clockout": 0,
-    "break_one": 0,
-    "break_two": 0
-}
+if item is not None:
+    timegap = pd.read_excel(item)
+    # Varialbe - set of all users
+    users = ("AJB002", "ALH002", "AXG004", "AXG006", "BBN001", "BJH001", "CXH003", "CXP001", "DLJ001", "DVA001", "DXN001", "DXK001", "EAY001", "JJS001", "JXA001", "KAN001", "LJM001", "MDL001", "NXP003", "PXL001", "SC303", "SC304", "SC306", "SPN001", "SXD001", "TXF001")
+    # Variable - set for all return carts, p3 carts, and bagntag cart
+    carts = ("#ACRT2A", "#ACRT2B", "#ACRT2C", "#ACRT2D", "#ACRT3A", "#ACRT3B", "#ACRT3C", "#ACRT3D", "RTNPLT1", "RTNFRONT", "P3RACK1A", "P3RACK1B", "P3RACK1C", "P3RACK1D", "#P3BINAA", "#P3BINAB", "#P3BINAC", "#P3BINBA", "#P3BINBB", "#P3BINBC", "#P3BINBD", "#P3BINCA", "#P3BINCB", "#P3BINCC", "#P3BINCD", "#P3BINDA", "#P3BINDB", "#P3BINDC", "#P3BINDD", "#P3BINDE", "#P3BINEA", "#P3BINEB", "#P3BINEC", "#P3BINED", "#P3BINEE", "#P3BINEF", "#P3BINFA", "#P3BINFB", "#P3BINFC", "#P3BINFD", "#P3BINFE", "#P3BINFF", "#P3BINFG", "#P3BINFH", "#P3BINGA", "#P3BINGB", "#P3BINGC", "#P3BINGD", "#P3BINGE", "#P3BINGF", "#P3BINGG", "#P3BINGH", "#P3BINHA", "#P3BINHB", "#P3BINHC", "#P3BINHD", "#P3BINHE", "#P3BINHG", "#P3BINHH", "#P3BINHI", "#P3BINHJ", "#P3BINHK", "#P3BINHL", "#P3BINHM", "#P3BINHN", "#P3BINHO", "#P3BINIA", "#P3BINIB", "#P3BINIC", "#P3BINID", "#P3BINIE", "#P3BINIF", "#P3BINIG", "#P3BINIH", "#P3BINII", "#P3BINIJ", "#P3BINIK", "#P3BINIL", "#P3BINIM", "#P3BININ", "#P3BINIO")
+    # Variable - set for bagntag locations
+    bagntag = ("#ACBAT", "#ACDMG", "#ACLABEL", "#ACOTHER")
 
-employee = {
-    "DVA001": employee_template.copy(),
-    "JXA001": employee_template.copy(),
-    "AJB002": employee_template.copy(),
-    "SXD001": employee_template.copy(),
-    "TXF001": employee_template.copy(),
-    "AXG006": employee_template.copy(),
-    "AXG004": employee_template.copy(),
-    "JJS001": employee_template.copy(),
-    "BJH001": employee_template.copy(),
-    "CXH003": employee_template.copy(),
-    "ALH002": employee_template.copy(),
-    "DLJ001": employee_template.copy(),
-    "DXK001": employee_template.copy(),
-    "PXL001": employee_template.copy(),
-    "MDL001": employee_template.copy(),
-    "LJM001": employee_template.copy(),
-    "SPN001": employee_template.copy(),
-    "DXN001": employee_template.copy(),
-    "BBN001": employee_template.copy(),
-    "KAN001": employee_template.copy(),
-    "CXP001": employee_template.copy(),
-    "NXP003": employee_template.copy(),
-    "EAY001": employee_template.copy(),
-    "SC303": employee_template.copy(),
-    "SC304": employee_template.copy(),
-    "SC306": employee_template.copy()
-}
+    # Variable - used to add row to user dataframe
+    row_to_copy = []
+    # Variable - list to hold user indexs
+    user_rows_cleaned = []
+    # Vailable - dictionary to store employee time
+    employee_template = {
+        "clockin": "",
+        "lunchout": 0,
+        "lunchin": 0,
+        "clockout": 0,
+        "break_one": 0,
+        "break_two": 0
+    }
 
+    employee = {
+        "DVA001": employee_template.copy(),
+        "JXA001": employee_template.copy(),
+        "AJB002": employee_template.copy(),
+        "SXD001": employee_template.copy(),
+        "TXF001": employee_template.copy(),
+        "AXG006": employee_template.copy(),
+        "AXG004": employee_template.copy(),
+        "JJS001": employee_template.copy(),
+        "BJH001": employee_template.copy(),
+        "CXH003": employee_template.copy(),
+        "ALH002": employee_template.copy(),
+        "DLJ001": employee_template.copy(),
+        "DXK001": employee_template.copy(),
+        "PXL001": employee_template.copy(),
+        "MDL001": employee_template.copy(),
+        "LJM001": employee_template.copy(),
+        "SPN001": employee_template.copy(),
+        "DXN001": employee_template.copy(),
+        "BBN001": employee_template.copy(),
+        "KAN001": employee_template.copy(),
+        "CXP001": employee_template.copy(),
+        "NXP003": employee_template.copy(),
+        "EAY001": employee_template.copy(),
+        "SC303": employee_template.copy(),
+        "SC304": employee_template.copy(),
+        "SC306": employee_template.copy()
+    }
 
-# Variable - dataframe to store initial excel file
-timegap = pd.read_excel('transactions.xls')
+    # Variable - dataframe to store initial excel file
+    #timegap = pd.read_excel('transactions.xls')
 
-# Variable - dataframe to store employee clock in time
-employeetime = pd.read_excel('dailytph.xlsx')
+    # Variable - dataframe to store employee clock in time
+    employeetime = pd.read_excel('./PythonServer/files/dailytph.xlsx')
 
-# Function Call - calls function to load employee time clock
-employee = get_time(employee, employeetime)
+    # Function Call - calls function to load employee time clock
+    employee = get_time(employee, employeetime)
 
-# Function Call - calls funtion clean file which removes all empty lines
-timegap = clean_file(timegap)
+    # Function Call - calls funtion clean file which removes all empty lines
+    timegap = clean_file(timegap)
 
-# Save File - saves the new dataframe
-timegap.to_excel('test.xlsx', index=False)
+    # Save File - saves the new dataframe
+    timegap.to_excel('./PythonServer/files/test.xlsx', index=False)
 
-# Variable - dataframe to store excel file
-timegap = pd.read_excel('test.xlsx')
-# Variable - column names
-column_names = ['DATETIME', 'ITEMID', 'TRANSACTION', 'SOURCEBIN', 'DESTBIN', 'QUANTITY', 'JOB']
-# Variable - master dataframe to hold cleaned data
-master_user = pd.DataFrame(columns=column_names)
+    # Variable - dataframe to store excel file
+    timegap = pd.read_excel('./PythonServer/files/test.xlsx')
+    # Variable - column names
+    column_names = ['DATETIME', 'ITEMID', 'TRANSACTION', 'SOURCEBIN', 'DESTBIN', 'QUANTITY', 'JOB']
+    # Variable - master dataframe to hold cleaned data
+    master_user = pd.DataFrame(columns=column_names)
 
-# Variable - lenght of file
-l = len(timegap) - 1
+    # Variable - lenght of file
+    l = len(timegap) - 1
 
-# For Loop - Clean data to remove unessassary lines
-for index, row in timegap.iterrows():
-    if index == l:
-        row_to_copy.append(row)
-        user = pd.DataFrame(row_to_copy, columns=['DATETIME', 'ITEMID', 'TRANSACTION', 'SOURCEBIN', 'DESTBIN', 'QUANTITY', 'JOB'])
-        user = clean_user(user, users, carts, bagntag)
-        master_user = pd.concat([master_user, user], ignore_index=True)
-
-        break
-
-    if row['DATETIME'] in users:
-        if row_to_copy == []:
+    # For Loop - Clean data to remove unessassary lines
+    for index, row in timegap.iterrows():
+        if index == l:
             row_to_copy.append(row)
-        else:
             user = pd.DataFrame(row_to_copy, columns=['DATETIME', 'ITEMID', 'TRANSACTION', 'SOURCEBIN', 'DESTBIN', 'QUANTITY', 'JOB'])
             user = clean_user(user, users, carts, bagntag)
             master_user = pd.concat([master_user, user], ignore_index=True)
-            
-            row_to_copy = []
+
+            break
+
+        if row['DATETIME'] in users:
+            if row_to_copy == []:
+                row_to_copy.append(row)
+            else:
+                user = pd.DataFrame(row_to_copy, columns=['DATETIME', 'ITEMID', 'TRANSACTION', 'SOURCEBIN', 'DESTBIN', 'QUANTITY', 'JOB'])
+                user = clean_user(user, users, carts, bagntag)
+                master_user = pd.concat([master_user, user], ignore_index=True)
+                
+                row_to_copy = []
+                row_to_copy.append(row)
+        else:
             row_to_copy.append(row)
-    else:
-        row_to_copy.append(row)
 
-# Save excel file
-master_user.to_excel('cleaned.xlsx', index=False)
+    # Save excel file
+    master_user.to_excel('.PythonServer/files/cleaned.xlsx', index=False)
 
-# Open cleaned file
-master_user = pd.read_excel('cleaned.xlsx')
+    # Open cleaned file
+    master_user = pd.read_excel('/PythonServer/files/cleaned.xlsx')
 
-# Funciton Call - calls function to add employees timecard into the cleaned transcations
-master_user = add_time(employee, master_user)
-master_user = master_user.sort_index().reset_index(drop=True)
-master_user = add_lunch(employee, master_user)
-master_user = master_user.sort_index().reset_index(drop=True)
-master_user = add_lunchin(employee, master_user)
-master_user = master_user.sort_index().reset_index(drop=True)
-master_user = add_clockout(employee, master_user)
-master_user = master_user.sort_index().reset_index(drop=True)
-master_user = add_breakone(employee, master_user)
-master_user = master_user.sort_index().reset_index(drop=True)
-master_user = add_breaktwo(employee, master_user)
-master_user = master_user.sort_index().reset_index(drop=True)
-# Save excel file
+    # Funciton Call - calls function to add employees timecard into the cleaned transcations
+    master_user = add_time(employee, master_user)
+    master_user = master_user.sort_index().reset_index(drop=True)
+    master_user = add_lunch(employee, master_user)
+    master_user = master_user.sort_index().reset_index(drop=True)
+    master_user = add_lunchin(employee, master_user)
+    master_user = master_user.sort_index().reset_index(drop=True)
+    master_user = add_clockout(employee, master_user)
+    master_user = master_user.sort_index().reset_index(drop=True)
+    master_user = add_breakone(employee, master_user)
+    master_user = master_user.sort_index().reset_index(drop=True)
+    master_user = add_breaktwo(employee, master_user)
+    master_user = master_user.sort_index().reset_index(drop=True)
+    # Save excel file
 
-master_user.to_excel('cleaned2.xlsx', index=False)
+    master_user.to_excel('./PythonServer/files/cleaned2.xlsx', index=False)
+    st.write(master_user)
